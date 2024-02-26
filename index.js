0d30a5985f84edb31bdeccef0d611787366c6050
Alvar0Gtz <161267357+Alvar0Gtz@users.noreply.github.com>
2024-02-26 18:54:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["East Custom 🚘","El mejor mecanico ✨"];
+const statusMessages = ["OrigenNetwork","Evolucion - Los Santos"];
 
 let currentIndex = 0;
 const channelId = '';
@@ -72,8 +72,8 @@ function updateStatusAndSendMessages() {
   const nextStatus = statusMessages[(currentIndex + 1) % statusMessages.length];
  
   client.user.setPresence({
-    activities: [{ name: currentStatus, type: ActivityType.PLAYING, url: "https://media.discordapp.net/attachments/1208011825652695070/1208104918691610654/AF795D33-5822-4117-8E38-2346D99E5C42.jpg?ex=65eb4ce2&is=65d8d7e2&hm=536945672fcbf132cc6c560757e3fcbf1cd4f6aaa1453765b406dcbdcdd40c23&=&format=webp&width=473&height=473", }],
-    status: 'dnd',
+    activities: [{ name: currentStatus, type: ActivityType.STREAMING, url: "https://discord.gg/URyekmrd", }],
+    status: 'idle',
   });
 
  
