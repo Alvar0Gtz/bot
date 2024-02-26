d830337dbd4b84d6c638ab352d8b264cafe3a800
Alvar0Gtz <161267357+Alvar0Gtz@users.noreply.github.com>
2024-02-26 20:06:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["OrigenNetwork"];
+const statusMessages = ["<a:3992purpleverify:845136214042214421> OrigenNetwork   "];
 
 let currentIndex = 0;
 const channelId = '';
